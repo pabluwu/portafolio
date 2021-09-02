f460d0d6b9f98d58ab139f330117e592efd653fd
Pablo López <lopez.pablo2305@gmail.com>
2021-09-02 17:53:04 -0400

    probando sistema login + template

--- mysite/process/views.py
+++ mysite/process/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hola mundo")
+
+def proyecto(request):
+    return render(request, 'process/project.html')
+
 # Create your views here.
