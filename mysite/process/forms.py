ae01c3a0c5f494bb539b1b5e004dc9057b7c79bf
Pablo López <lopez.pablo2305@gmail.com>
2021-09-08 01:29:39 -0300

    Agregar flujos de tareas y tareas

---
+++ mysite/process/forms.py
@@ -0,0 +1,33 @@
+from django import forms 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group, Permission
+from django.contrib.admin.widgets import FilteredSelectMultiple
+
+from . import models
+
+class CustomUserCreationForm(UserCreationForm):
+    pass
+
+class FlujoTareaForm(forms.ModelForm):
+
+    class Meta:
+        model = models.FlujoTarea
+        fields = '__all__'
+
+class TareaForm(forms.ModelForm):
+    
+    class Meta:
+        model = models.Tarea
+        fields = '__all__'
+
+class UserGroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = '__all__'
+        widgets = {
+            'permissions': FilteredSelectMultiple("Permission", False, attrs={'rows':'2'}),
+        }
+
+
+
+    
--- mysite/process/models.py
+++ mysite/process/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from datetime import datetime, date
 # Create your models here.
+class FlujoTarea(models.Model):
+    nombre = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+
+class Tarea(models.Model):
+    nombre = models.CharField(max_length=50, blank=False)
+    descripcion = models.TextField(blank=False)
+    realizado = models.BooleanField(default=False)
+    fechaCreacion = models.DateTimeField(auto_now_add=True, auto_now=False)
+    
+    def __str__(self):
+        return self.nombre
+
+class FlujoTarea_Tarea(models.Model):
+    FlujoTarea = models.ForeignKey(FlujoTarea, on_delete=models.PROTECT)
+    Tarea = models.ForeignKey(Tarea, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.FlujoTarea
+
