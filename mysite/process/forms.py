b49cf1a2538b279cf20d9a940a1347b95d9f9397
Pablo López <lopez.pablo2305@gmail.com>
2021-09-17 04:39:00 -0300

    validación registro usuario

--- mysite/process/forms.py
+++ mysite/process/forms.py
@@ -2,11 +2,22 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group, Permission, User
 from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.forms import ValidationError
 
 from . import models
 
-class CustomUserCreationForm(UserCreationForm):
-    pass
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Confirmar contraseña', widget=forms.PasswordInput)
+    
+
+    class Meta:
+        model = User
+        fields=['username','first_name','last_name','email']
+
+   
+
 
 class FlujoTareaForm(forms.ModelForm):
 
@@ -28,6 +39,14 @@ class UserGroupForm(forms.ModelForm):
             'permissions': FilteredSelectMultiple("Permission", False, attrs={'rows':'2'}),
         }
 
+class GroupForm(forms.Form):
+    nombre = forms.CharField(max_length=50)
+
+class UsuarioFormulario(forms.Form):
+    username = forms.CharField(max_length=50)
+    nombre = forms.CharField(max_length=50)
+    apellido = forms.CharField(max_length=50)
+    email = forms.CharField(max_length=50)
 
 
     
--- mysite/process/views.py
+++ mysite/process/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
@@ -14,25 +14,7 @@ def index(request):
 def proyecto(request):
     return render(request, 'process/project.html')
 
-@login_required()
-def agregar_flujo_tarea(request):
-    if request.user.is_superuser:
-        data = {
-            'form': forms.FlujoTareaForm()
-        }
-        if request.method == 'POST':
-            formulario = forms.FlujoTareaForm(data=request.POST, files=request.FILES)
-            if formulario.is_valid():
-                formulario.save()
-                data["mensaje"] = "Guardado correctamente."
-            else:
-                data["form"] = formulario
-        
-        return render(request, 'process/flujo_tarea/agregar_flujo_tarea.html', data)
-    else:
-        return render(request,'process/error_permiso.html')
-
-
+## Gestión de tareas
 @login_required()
 def agregar_tarea(request):
     data= {
@@ -56,18 +38,6 @@ def listar_tareas(request):
     return render(request, 'process/tarea/listar_tarea.html', data)
 
 @login_required()
-def listar_flujo_tareas(request):
-    if request.user.is_superuser:
-        flujo_tareas = models.FlujoTarea.objects.all()
-        data = {
-            'flujo_tareas':flujo_tareas
-        }
-        return render(request, 'process/flujo_tarea/listar_flujo_tareas.html', data)
-    else:
-        return render(request,'process/error_permiso.html')
-
-
-@login_required()
 def modificar_tarea(request, id):
     tarea = get_object_or_404(models.Tarea, id=id)
 
@@ -82,6 +52,37 @@ def modificar_tarea(request, id):
             return redirect(to="listar_tareas")
         data["form"] = formulario
     return render(request,'process/tarea/modificar_tarea.html', data)
+
+## Gestión flujo de tareas
+
+@login_required()
+def agregar_flujo_tarea(request):
+    if request.user.is_superuser:
+        data = {
+            'form': forms.FlujoTareaForm()
+        }
+        if request.method == 'POST':
+            formulario = forms.FlujoTareaForm(data=request.POST, files=request.FILES)
+            if formulario.is_valid():
+                formulario.save()
+                data["mensaje"] = "Guardado correctamente."
+            else:
+                data["form"] = formulario
+        
+        return render(request, 'process/flujo_tarea/agregar_flujo_tarea.html', data)
+    else:
+        return render(request,'process/error_permiso.html')
+
+@login_required()
+def listar_flujo_tareas(request):
+    if request.user.is_superuser:
+        flujo_tareas = models.FlujoTarea.objects.all()
+        data = {
+            'flujo_tareas':flujo_tareas
+        }
+        return render(request, 'process/flujo_tarea/listar_flujo_tareas.html', data)
+    else:
+        return render(request,'process/error_permiso.html')
 
 @login_required()
 def modificar_flujo_tarea(request, id):
@@ -102,18 +103,88 @@ def modificar_flujo_tarea(request, id):
     else:
         return render(request,'process/error_permiso.html')
 
+##Gestión de usuarios.
 @login_required()
 def registrar_usuario(request):
     if request.user.is_superuser:
         data={
-            'form': forms.CustomUserCreationForm()
+            'form': forms.UserCreationForm()
         }
 
         if request.method=='POST':
-            formulario = forms.CustomUserCreationForm(data=request.POST, files=request.FILES)
+            formulario = forms.UserCreationForm(data=request.POST)
+            print("formulario")
+            username = formulario['username'].value()
+            password1 = formulario['password1'].value()
+            password2 = formulario['password2'].value()
+            if len(username) >= 5:
+                print(username)
+                if len(password1) > 7:
+                    if password1 == password2:
+                        if formulario.is_valid():
+                            formulario.save()
+                            data["mensaje"] = "Guardado correctamente."
+                            print("guardado")
+                    else:
+                        data["mensaje"] = "Contraseñas no coinciden"
+                        data["form"] = formulario
+                else:
+                    data["mensaje"] = "Contraseña demasiado corta"
+                    data["form"] = formulario
+            else:
+                data["mensaje"] = "Nombre de usuario debe ser mayor a 5 caracteres."
+                data["form"] = formulario
+        return render(request,'registration/register_user.html', data) 
+    else:
+        return render(request,'process/error_permiso.html')
+
+@login_required()
+def listar_usuarios(request):
+    if request.user.is_superuser:
+        User = get_user_model()
+        users = User.objects.all()
+        data={
+            'users':users 
+        }
+        return render(request, 'registration/listar_usuarios.html', data)
+    else:
+        return render(request,'process/error_permiso.html')
+
+@login_required()
+def modificar_usuario_admin(request, id):
+    if request.user.is_superuser:
+        User = get_user_model()
+        usuario = get_object_or_404(User, id=id)
+        data_form={'username':usuario.get_username(),
+        'nombre':usuario.get_short_name(),
+        'apellido':usuario.last_name,
+        'email':usuario.email}
+        print(usuario)
+        data={
+            'form': forms.UsuarioFormulario(data_form)
+        }
+        if request.method=='POST':
+            formulario = forms.UsuarioFormulario(data=request.POST, files=request.FILES)
             if formulario.is_valid():
-                formulario.save()
-        return render(request,'registration/register_user.html', data)
+                usuario.first_name=formulario['nombre'].value()
+                usuario.last_name=formulario['apellido'].value()
+                usuario.email=formulario['email'].value()
+                usuario.save()
+                data["mensaje"] = "Modificado correctamente."
+
+        return render(request,'registration/modificar_usuario.html', data)
+    else:
+        return render(request,'process/error_permiso.html')
+
+
+##Gestión de grupos y roles POR HACER.
+@login_required()
+def agregar_grupo(request):
+    if request.user.is_superuser:
+        data={
+            'form': forms.GroupForm()
+        }
+        return render(request,'process/grupos_roles/agregar_grupo.html', data)
     else:
         return render(request,'process/error_permiso.html')
 
